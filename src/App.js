98fbecdb9dfb52c48315becb459c89490ea2c257
raghavsrirama <raghav.srirama@gmai.com>
2025-03-05 22:07:02 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          This is Raghu's first react project. Welcome Raghu!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Raghu, understand the basics of react.
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
